f34938d1cc4e76de9c260fe169a77c3b9fdf4ae6
Frey Khademi <frey.khademi@gmail.com>
2020-10-23 23:22:16 +0200

    fix

--- data.tf
+++ data.tf
@@ -3,7 +3,7 @@ data "dns_a_record_set" "fqdn" {
 }
 
 data "azurerm_subnet" "spoke_azure_1" {
-  name                 = module.spoke_azure_1.vnet.subnets[3].name
-  virtual_network_name = module.spoke_azure_1.vnet.name
-  resource_group_name  = split(":", module.spoke_azure_1.vnet.vpc_id)[1]
+  name                 = module.spoke_azure_fra.vnet.subnets[3].name
+  virtual_network_name = module.spoke_azure_fra.vnet.name
+  resource_group_name  = split(":", module.spoke_azure_fra.vnet.vpc_id)[1]
 }
