aa893cc54d3a8d157fe42ddf7da5983772138039
Frey Khademi <frey.khademi@gmail.com>
2020-10-23 23:41:07 +0200

    fix dns

--- data.tf
+++ data.tf
@@ -1,7 +1,10 @@
 data "dns_a_record_set" "fqdn" {
   host = var.onprem_fqdn
 }
-
+data "aws_route53_zone" "pub" {
+  name         = "avxlab.de"
+  private_zone = false
+}
 data "azurerm_subnet" "spoke_azure_fra" {
   name                 = module.spoke_azure_fra.vnet.subnets[3].name
   virtual_network_name = module.spoke_azure_fra.vnet.name
