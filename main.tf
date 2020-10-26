ef4e2eeb1309384d5494f0f6e21ad1d3765f8aee
Frey Khademi <frey.khademi@gmail.com>
2020-10-26 10:23:29 +0100

    add spokes

--- main.tf
+++ main.tf
@@ -101,7 +101,116 @@ resource "aws_route53_record" "gcp1" {
   ttl     = "1"
   records = [module.gcp1.vm.network_interface[0].network_ip]
 }
+module "spoke_aws_4" {
+  source  = "terraform-aviatrix-modules/aws-spoke/aviatrix"
+  version = "2.0.0"
 
+  name          = "spoke4"
+  cidr          = "10.40.4.0/24"
+  region        = var.region1["region"]
+  account       = var.aws_account_name
+  transit_gw    = module.transit_firenet_1.transit_gateway.gw_name
+  ha_gw         = false
+}
+module "spoke_aws_5" {
+  source  = "terraform-aviatrix-modules/aws-spoke/aviatrix"
+  version = "2.0.0"
+
+  name          = "spoke5"
+  cidr          = "10.40.5.0/24"
+  region        = var.region1["region"]
+  account       = var.aws_account_name
+  transit_gw    = module.transit_firenet_1.transit_gateway.gw_name
+  ha_gw         = false
+}
+module "spoke_aws_6" {
+  source  = "terraform-aviatrix-modules/aws-spoke/aviatrix"
+  version = "2.0.0"
+
+  name          = "spoke6"
+  cidr          = "10.40.6.0/24"
+  region        = var.region1["region"]
+  account       = var.aws_account_name
+  transit_gw    = module.transit_firenet_1.transit_gateway.gw_name
+  ha_gw         = false
+}
+module "spoke_aws_7" {
+  source  = "terraform-aviatrix-modules/aws-spoke/aviatrix"
+  version = "2.0.0"
+
+  name          = "spoke7"
+  cidr          = "10.40.7.0/24"
+  region        = var.region1["region"]
+  account       = var.aws_account_name
+  transit_gw    = module.transit_firenet_1.transit_gateway.gw_name
+  ha_gw         = false
+}
+module "spoke_aws_8" {
+  source  = "terraform-aviatrix-modules/aws-spoke/aviatrix"
+  version = "2.0.0"
+
+  name          = "spoke8"
+  cidr          = "10.40.8.0/24"
+  region        = var.region1["region"]
+  account       = var.aws_account_name
+  transit_gw    = module.transit_firenet_1.transit_gateway.gw_name
+  ha_gw         = false
+}
+module "spoke_aws_9" {
+  source  = "terraform-aviatrix-modules/aws-spoke/aviatrix"
+  version = "2.0.0"
+
+  name          = "spoke9"
+  cidr          = "10.40.9.0/24"
+  region        = var.region1["region"]
+  account       = var.aws_account_name
+  transit_gw    = module.transit_firenet_1.transit_gateway.gw_name
+  ha_gw         = false
+}
+module "spoke_aws_10" {
+  source  = "terraform-aviatrix-modules/aws-spoke/aviatrix"
+  version = "2.0.0"
+
+  name          = "spoke10"
+  cidr          = "10.40.10.0/24"
+  region        = var.region1["region"]
+  account       = var.aws_account_name
+  transit_gw    = module.transit_firenet_1.transit_gateway.gw_name
+  ha_gw         = false
+}
+module "spoke_aws_11" {
+  source  = "terraform-aviatrix-modules/aws-spoke/aviatrix"
+  version = "2.0.0"
+
+  name          = "spoke11"
+  cidr          = "10.40.11.0/24"
+  region        = var.region1["region"]
+  account       = var.aws_account_name
+  transit_gw    = module.transit_firenet_1.transit_gateway.gw_name
+  ha_gw         = false
+}
+module "spoke_aws_12" {
+  source  = "terraform-aviatrix-modules/aws-spoke/aviatrix"
+  version = "2.0.0"
+
+  name          = "spoke12"
+  cidr          = "10.40.12.0/24"
+  region        = var.region1["region"]
+  account       = var.aws_account_name
+  transit_gw    = module.transit_firenet_1.transit_gateway.gw_name
+  ha_gw         = false
+}
+module "spoke_aws_13" {
+  source  = "terraform-aviatrix-modules/aws-spoke/aviatrix"
+  version = "2.0.0"
+
+  name          = "spoke13"
+  cidr          = "10.40.13.0/24"
+  region        = var.region1["region"]
+  account       = var.aws_account_name
+  transit_gw    = module.transit_firenet_1.transit_gateway.gw_name
+  ha_gw         = false
+}
 /* ####
 ## LH POC
 ####
